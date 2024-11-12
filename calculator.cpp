aba9622145e65d5481624594da9ca9cccde7422b
Meriem-ghrissi <ghrissimeriem0@gmail.com>
2024-11-12 23:45:22 +0100

    Updated code from VSCode

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,31 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+void cal(){
+    int a,b;
+    char op;
+    cout<<"give the first number"<<endl;
+    cin>>a;
+    cout<<"give the operator"<<endl;
+    cin>>op;
+    cout<<"give the second number"<<endl;
+    cin>>b;
+    switch(op){
+        case('+'):
+          cout<<a+b<<endl;
+          break;
+        case('-'):
+          cout<<a-b<<endl;
+          break;
+        case('*'):
+          cout<<a*b<<endl;
+          break;
+        default:
+          cout<<"rewrite the operator"<<endl;
+          
+    }  }
+int main() {
+    cal();
 
+    
+}
